6fbd359bf742fb5ee523b73e93d4d4709794d170
Denny <dennynj4748@gmail.com>
2026-01-02 09:35:16 +0530

    fix: correct sidebar behavior

--- layout/AppSidebar.tsx
+++ layout/AppSidebar.tsx
@@ -114,6 +114,12 @@ const AppSidebar: React.FC = () => {
     [pathname]
   );
 
+  const handleNavClick = () => {
+    if (isMobileOpen) {
+      toggleMobileSidebar();
+    }
+  };
+
   return (
     <aside
       className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
@@ -136,7 +142,7 @@ const AppSidebar: React.FC = () => {
       >
         <Link
           href="/"
-          onClick={toggleMobileSidebar}
+          onClick={handleNavClick}
           className="flex items-center gap-3"
         >
           {/* Logo */}
@@ -167,7 +173,7 @@ const AppSidebar: React.FC = () => {
             <li>
               <Link
                 href={homeNavItem.path}
-                onClick={toggleMobileSidebar}
+                onClick={handleNavClick}
                 className={`menu-item group ${
                   isActive(homeNavItem.path)
                     ? "menu-item-active"
@@ -219,7 +225,7 @@ const AppSidebar: React.FC = () => {
                     <li key={nav.name}>
                       <Link
                         href={nav.path}
-                        onClick={toggleMobileSidebar}
+                        onClick={handleNavClick}
                         className={`menu-item group ${
                           isActive(nav.path)
                             ? "menu-item-active"
