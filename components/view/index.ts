a0af47d03c4569b2b0ea247484e5b1df32f09f8d
Denny <dennynj4748@gmail.com>
2026-01-08 15:13:35 +0530

    feat(ui): add new view components ViewRating

--- components/view/index.ts
+++ components/view/index.ts
@@ -3,4 +3,5 @@ export { default as ViewField } from "./ViewField";
 export { default as ViewImage } from "./ViewImage";
 export { default as ViewList } from "./ViewList";
 export { default as ViewMediaGrid } from "./ViewMediaGrid";
+export { default as ViewRating } from "./ViewRating";
 export { default as ViewSection } from "./ViewSection";
